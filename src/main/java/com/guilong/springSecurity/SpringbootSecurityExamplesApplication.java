fe3a05d50fbf3543d1523db479077a2a96303bfd
guilongli <guilongli@ymail.com>
2017-10-11 11:06:01 -0400

    add all

---
+++ src/main/java/com/guilong/springSecurity/SpringbootSecurityExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.guilong.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityExamplesApplication.class, args);
+	}
+}
---
+++ src/main/java/com/guilong/springSecurity/controllers/HelloController.java
@@ -0,0 +1,22 @@
+package com.guilong.springSecurity.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    
+	@RequestMapping("/")
+	 public String index() {
+		 return "index";
+	 }
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
